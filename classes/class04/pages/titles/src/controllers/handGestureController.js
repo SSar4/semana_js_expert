13538bfb9ce4c476782022079fb74eaff46cd285
j.a.s <aras.strong8@gmail.com>
2023-03-03 12:53:58 -0300

    aula 03

---
+++ classes/class04/pages/titles/src/controllers/handGestureController.js
@@ -0,0 +1,71 @@
+import { prepareRunChecker } from "../../../../lib/shared/util.js";
+
+const { shouldRun: scrollShouldRun } = prepareRunChecker({ timerDelay: 200 });
+const { shouldRun: clickShouldRun } = prepareRunChecker({ timerDelay: 200 });
+export default class HandGestureContoller {
+  #view;
+  #service;
+  #camera;
+  #lastDirection = {
+    direction: "",
+    y: 0,
+  };
+  constructor({ view, service, camera }) {
+    this.#view = view;
+    this.#service = service;
+    this.#camera = camera;
+  }
+  async init() {
+    await this.#loop();
+  }
+
+  #scrollPage(direction) {
+    const pixelsPerScroll = 400;
+    if (this.#lastDirection.direction === direction) {
+      this.#lastDirection.y =
+        direction === "scroll-down"
+          ? this.#lastDirection.y + pixelsPerScroll
+          : this.#lastDirection.y - pixelsPerScroll;
+    } else {
+      this.#lastDirection.direction = direction;
+    }
+    this.#view.scrollPage(this.#lastDirection.y);
+  }
+  async #estimateHands() {
+    try {
+      const hands = await this.#service.estimateHands(this.#camera.video);
+      this.#view.clearCanvas()
+      if (hands && hands?.length) {
+        this.#view.drawResults(hands)
+      }
+      for await (const { event, x, y } of this.#service.detectGesture(hands)) {
+        if (event.includes("scroll")) {
+          if (!scrollShouldRun()) continue;
+          this.#scrollPage(event);
+        }
+        if (event.includes("click")) {
+          this.#view.clickOnElement(x, y)
+          if (!clickShouldRun()) continue
+          
+        }
+
+        if (hands?.length === 2) console.log("🖐🖐");
+        if (hands?.length === 1) console.log("🖐", hands[0].handedness);
+      }
+    } catch (error) {
+      console.error("error", error);
+    }
+  }
+
+  async #loop() {
+    await this.#service.initializeDetector();
+    await this.#estimateHands();
+    //o view é deste contexto
+    this.#view.loop(this.#loop.bind(this));
+  }
+
+  static async initialize(deps) {
+    const controller = new HandGestureContoller(deps);
+    return controller.init();
+  }
+}
---
+++ classes/class04/pages/titles/src/utils/util.js
@@ -0,0 +1,11 @@
+import { knowGestures, gestureStrings } from "./gestures.js"
+
+const fingerLookupIndices = {
+    tumb: [0, 1, 2, 3, 4],
+    indexFinger:[0, 5, 6, 7, 8],
+    middle: [0, 9, 10, 11, 12],
+    ring: [0, 13, 14, 15, 16],
+    pink: [0, 17, 18, 19, 20]
+}
+
+export { fingerLookupIndices, knowGestures, gestureStrings }
